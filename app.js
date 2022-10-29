49be1571c02a63eb72165dfe5436ef0308dbedf6
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-28 23:39:43 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const Web3 = require('web3');
+const solc = require("solc");
+const fs = require("fs");
+
+const PROVIDER_URL = "https://goerli.infura.io/v3/08ca1c5dddd34508a9cba3a337ae801c";
+const web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER_URL));
+
+const contractCode = fs.readFileSync("./ArrayOfFacts.sol").toString();
